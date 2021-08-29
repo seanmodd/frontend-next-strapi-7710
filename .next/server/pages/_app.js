bac67f88a46f8045eb5123a9083ffa60feb2b4c6
Sean <sean@vestaridefair.com>
2021-08-29 14:59:55 -0700

    trying to fix git

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,67 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 3426:
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/react");
+
+/***/ }),
+
+/***/ 8547:
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/theme-tools");
+
+/***/ }),
+
+/***/ 762:
+/***/ ((module) => {
+
+module.exports = require("framer-motion");
+
+/***/ }),
+
+/***/ 7620:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 701:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 9297:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 5282:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [544,177], () => (__webpack_exec__(2177)));
+module.exports = __webpack_exports__;
+
+})();
