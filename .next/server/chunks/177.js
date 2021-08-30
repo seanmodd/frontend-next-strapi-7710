49c03f2571a2938ec8dca1dba9895f03dd8c724e
Sean <sean@vestaridefair.com>
2021-08-29 23:12:35 -0700

    fixing it one last time on night of aug 29

--- .next/server/chunks/177.js
+++ .next/server/chunks/177.js
@@ -66,41 +66,41 @@ const theme = (0,react_.extendTheme)({
   styles: {
     global: props => ({
       h1: {
-        textShadow: '-1px 1px 1px #ff00007e',
-        fontFamily: 'Poppins, sans-serif',
-        fontSize: '4.5rem',
-        textAlign: 'center',
-        color: props.colorMode === 'dark' ? 'red.400' : '#ff00ea',
-        transition: 'all 0.1s ease-in-out',
-        fontWeight: 'semibold',
-        letterSpacing: '-0.02em',
-        lineHeight: '1.5',
-        padding: '0 1rem',
-        textShadow: props.colorMode === 'dark' ? '2px 1px red' : '0px 1px#0000006c',
+        textShadow: "-1px 1px 1px #ff00007e",
+        fontFamily: "Poppins, sans-serif",
+        fontSize: "4.5rem",
+        textAlign: "center",
+        color: props.colorMode === "dark" ? "red.400" : "#ff00ea",
+        transition: "all 0.1s ease-in-out",
+        fontWeight: "semibold",
+        letterSpacing: "-0.02em",
+        // lineHeight: '1.5',
+        padding: "0 1rem",
+        textShadow: props.colorMode === "dark" ? "2px 1px red" : "0px 1px#0000006c",
         _hover: {
-          letterSpacing: '0.05em'
+          letterSpacing: "0.05em"
         }
       },
       body: {
-        color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
-        backgroundColor: props.colorMode === 'dark' ? 'gray.900' : 'gray.100',
-        transition: 'all 0.1s ease-in-out'
+        color: props.colorMode === "dark" ? "gray.50" : "gray.800",
+        backgroundColor: props.colorMode === "dark" ? "gray.900" : "gray.100",
+        transition: "all 0.1s ease-in-out"
       },
       input: {
-        backgroundColor: props.colorMode === 'dark' ? 'gray.900' : 'gray.100',
-        boxShadow: 'md',
-        margin: '10px',
-        padding: '10px',
-        border: props.colorMode === 'dark' ? '0.1px solid #88888886' : '0.1px solid #f0121285',
-        transition: 'all 0.1s ease-in-out',
-        borderRadius: '5px',
+        backgroundColor: props.colorMode === "dark" ? "gray.900" : "gray.100",
+        boxShadow: "md",
+        margin: "10px",
+        padding: "10px",
+        border: props.colorMode === "dark" ? "0.1px solid #88888886" : "0.1px solid #f0121285",
+        transition: "all 0.1s ease-in-out",
+        borderRadius: "5px",
         _hover: {
-          transition: 'ease-in-out, width .35s ease-in-out',
-          backgroundColor: props.colorMode === 'dark' ? 'gray.700' : 'blue.50',
-          boxShadow: props.colorMode === 'dark' ? '0 0 1px 1px #dee4ef' : '0 0 2px 4px #dee4ef'
+          transition: "ease-in-out, width .35s ease-in-out",
+          backgroundColor: props.colorMode === "dark" ? "gray.700" : "blue.50",
+          boxShadow: props.colorMode === "dark" ? "0 0 1px 1px #dee4ef" : "0 0 2px 4px #dee4ef"
         },
         _focus: {
-          transition: 'all 0.1s ease-in-out'
+          transition: "all 0.1s ease-in-out"
         }
       },
       // div: {
@@ -109,39 +109,39 @@ const theme = (0,react_.extendTheme)({
       // },
       a: {
         // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
-        fontWeight: '700',
-        transition: 'all 0.1s ease-in-out',
-        textTransform: 'uppercase',
-        textAlign: 'center',
-        color: props.colorMode === 'dark' ? 'gray.50' : '#ff00ea',
-        textDecoration: 'underline',
+        fontWeight: "700",
+        transition: "all 0.1s ease-in-out",
+        textTransform: "uppercase",
+        textAlign: "center",
+        color: props.colorMode === "dark" ? "gray.50" : "#ff00ea",
+        textDecoration: "underline",
         _hover: {
-          textShadow: '-1px 1px 5px #30303029',
-          color: props.colorMode === 'dark' ? '#0077ff' : '#0077ff',
-          transition: 'all 0.1s ease-in-out'
+          textShadow: "-1px 1px 5px #30303029",
+          color: props.colorMode === "dark" ? "#0077ff" : "#0077ff",
+          transition: "all 0.1s ease-in-out"
         }
       },
       button: {
-        px: '4',
-        py: '3',
-        m: '2',
-        textAlign: 'center',
+        px: "4",
+        py: "3",
+        m: "2",
+        textAlign: "center",
         // bg: 'gray.700',
         // color: 'blue.50',
-        bg: props.colorMode === 'dark' ? 'gray.300' : 'gray.300',
+        bg: props.colorMode === "dark" ? "gray.300" : "gray.300",
         // boxShadow: '3px 3px 2px 1px rgba(0, 0, 255, .2);',
-        boxShadow: props.colorMode === 'dark' ? '1px 1px 1px 1px #aaaaaa47;' : '1px 1px 1px 1px #0000ff0;',
-        color: props.colorMode === 'dark' ? '#6f00ff' : '#6f00ff',
+        boxShadow: props.colorMode === "dark" ? "1px 1px 1px 1px #aaaaaa47;" : "1px 1px 1px 1px #0000ff0;",
+        color: props.colorMode === "dark" ? "#6f00ff" : "#6f00ff",
         // fontWeight: 'normal',
         // textShadow: '1px 1px 0.2px rgba(0, 0, 255, .2);',
         // borderWidth: '1px',
-        borderRadius: '10px',
-        transition: 'all 0.5s ease-in-out',
+        borderRadius: "10px",
+        transition: "all 0.5s ease-in-out",
         // borderColor: 'gray.700',
         _hover: {
           // borderColor: '#0022e4',
           // bg: 'purple.400',
-          transition: 'all 0.5s ease-in-out' // color: props.colorMode === 'dark' ? '#ff00ea' : '#ff00ea',
+          transition: "all 0.5s ease-in-out" // color: props.colorMode === 'dark' ? '#ff00ea' : '#ff00ea',
           // fontWeight: 'bold',
           // fontSize: '18px',
           // boxShadow:
@@ -153,31 +153,31 @@ const theme = (0,react_.extendTheme)({
           // borderColor: '#0022e4',
 
         },
-        transition: 'all 0.5s ease-in-out'
+        transition: "all 0.5s ease-in-out"
       }
     })
   },
   components: {
     Heading: {
       baseStyle: {
-        fontSize: '1.5rem',
-        fontWeight: 'bold',
-        lineHeight: '1.5',
-        fontFamily: 'Poppins, sans-serif' // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        lineHeight: "1.5",
+        fontFamily: "Poppins, sans-serif" // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
 
       },
       variants: {
-        'with-shadow': {// bg: 'red.400',
+        "with-shadow": {// bg: 'red.400',
           // boxShadow: '0 0 2px 2px #efdfde',
         },
         solid: props => ({
-          bg: (0,theme_tools_.mode)('blue.500', 'red.500')(props),
+          bg: (0,theme_tools_.mode)("blue.500", "red.500")(props),
           // bg: 'gray.300',
-          color: props.colorMode === 'dark' ? 'red.500' : 'blue.800',
+          color: props.colorMode === "dark" ? "red.500" : "blue.800",
           _hover: {
-            bg: props.colorMode === 'dark' ? 'blue.500' : 'red.500',
-            bg: 'gray.300',
-            textShadow: '0 3px 0 #a0a0a07e' // borderRadius: '15px',
+            bg: props.colorMode === "dark" ? "blue.500" : "red.500",
+            bg: "gray.300",
+            textShadow: "0 3px 0 #a0a0a07e" // borderRadius: '15px',
 
           }
         })
@@ -185,24 +185,24 @@ const theme = (0,react_.extendTheme)({
     },
     Text: {
       baseStyle: {
-        fontSize: '1.5rem',
-        fontWeight: 'bold',
-        lineHeight: '1.5',
-        fontFamily: 'Poppins, sans-serif' // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        lineHeight: "1.5",
+        fontFamily: "Poppins, sans-serif" // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
 
       },
       variants: {
-        'with-shadow': {// bg: 'red.400',
+        "with-shadow": {// bg: 'red.400',
           // boxShadow: '0 0 2px 2px #efdfde',
         },
         solid: props => ({
-          bg: (0,theme_tools_.mode)('blue.500', 'red.500')(props),
+          bg: (0,theme_tools_.mode)("blue.500", "red.500")(props),
           // bg: 'gray.300',
-          color: props.colorMode === 'dark' ? 'red.500' : 'blue.800',
+          color: props.colorMode === "dark" ? "red.500" : "blue.800",
           _hover: {
-            bg: props.colorMode === 'dark' ? 'blue.500' : 'red.500',
-            bg: 'gray.300',
-            textShadow: '0 3px 0 #a0a0a07e' // borderRadius: '15px',
+            bg: props.colorMode === "dark" ? "blue.500" : "red.500",
+            bg: "gray.300",
+            textShadow: "0 3px 0 #a0a0a07e" // borderRadius: '15px',
 
           }
         })
@@ -212,7 +212,7 @@ const theme = (0,react_.extendTheme)({
       // setup light/dark mode component defaults
       baseStyle: props => ({
         dialog: {
-          bg: (0,theme_tools_.mode)('red.500', 'blue.500')(props)
+          bg: (0,theme_tools_.mode)("red.500", "blue.500")(props)
         }
       })
     },
@@ -224,52 +224,52 @@ const theme = (0,react_.extendTheme)({
     // },
     Button: {
       baseStyle: {
-        fontWeight: 'normal',
+        fontWeight: "normal",
         // borderColor: '#ff0000',
-        bg: 'gray.50',
-        textShadow: '0 3px 0 #a0a0a07e' // borderWidth: '2px',
+        bg: "gray.50",
+        textShadow: "0 3px 0 #a0a0a07e" // borderWidth: '2px',
         // borderRadius: '15px',
 
       },
       sizes: {
         xl: {
-          h: '56px',
-          fontSize: 'lg',
-          px: '32px'
+          h: "56px",
+          fontSize: "lg",
+          px: "32px"
         },
         lg: {
-          h: '48px',
-          fontSize: 'lg',
-          px: '24px'
+          h: "48px",
+          fontSize: "lg",
+          px: "24px"
         },
         md: {
-          h: '40px',
-          fontSize: 'lg',
-          px: '20px'
+          h: "40px",
+          fontSize: "lg",
+          px: "20px"
         },
         sm: {
-          h: '32px',
-          fontSize: 'lg',
-          px: '16px'
+          h: "32px",
+          fontSize: "lg",
+          px: "16px"
         },
         xs: {
-          h: '28px',
-          fontSize: 'lg',
-          px: '14px'
+          h: "28px",
+          fontSize: "lg",
+          px: "14px"
         }
       },
       variants: {
-        'with-shadow': {// bg: 'red.400',
+        "with-shadow": {// bg: 'red.400',
           // boxShadow: '0 0 2px 2px #efdfde',
         },
         solid: props => ({
           // bg: props.colorMode === 'dark' ? 'blue.500' : 'red.500',
-          bg: 'gray.300',
+          bg: "gray.300",
           // color: props.colorMode === 'dark' ? 'gray.50' : 'gray.800',
           _hover: {
             // bg: props.colorMode === 'dark' ? 'blue.500' : 'red.500',
-            bg: 'gray.300',
-            textShadow: '0 3px 0 #a0a0a07e' // borderRadius: '15px',
+            bg: "gray.300",
+            textShadow: "0 3px 0 #a0a0a07e" // borderRadius: '15px',
 
           }
         })
@@ -279,29 +279,29 @@ const theme = (0,react_.extendTheme)({
 }); // ! BELOW IS MY THEME FROM GEIST UI!!!!
 
 const myTheme = {
-  type: 'Custom',
+  type: "Custom",
   palette: {
-    accents_1: '#111',
-    accents_2: '#333',
-    accents_3: '#444',
-    accents_4: '#666',
-    accents_5: '#888',
-    accents_6: '#999',
-    accents_7: '#eaeaea',
-    accents_8: '#fafafa',
-    background: '#000',
-    foreground: '#fff',
-    selection: '#f81ce5',
-    secondary: '#888',
-    code: '#79ffe1',
-    border: '#333',
-    link: '#3291ff'
+    accents_1: "#111",
+    accents_2: "#333",
+    accents_3: "#444",
+    accents_4: "#666",
+    accents_5: "#888",
+    accents_6: "#999",
+    accents_7: "#eaeaea",
+    accents_8: "#fafafa",
+    background: "#000",
+    foreground: "#fff",
+    selection: "#f81ce5",
+    secondary: "#888",
+    code: "#79ffe1",
+    border: "#333",
+    link: "#3291ff"
   },
   expressiveness: {
-    dropdownBoxShadow: '0 0 0 1px #333',
-    shadowSmall: '0 0 0 1px #333',
-    shadowMedium: '0 0 0 1px #333',
-    shadowLarge: '0 0 0 1px #333',
+    dropdownBoxShadow: "0 0 0 1px #333",
+    shadowSmall: "0 0 0 1px #333",
+    shadowMedium: "0 0 0 1px #333",
+    shadowLarge: "0 0 0 1px #333",
     portalOpacity: 0.75
   }
 };
