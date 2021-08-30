d37f10b94f7d712180ff3c6426130264012d54e4
Sean <sean@vestaridefair.com>
2021-08-30 10:34:07 -0700

    changed category slug page completely

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -4,48 +4,52 @@ import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import { Box } from "@chakra-ui/react"
 
-const Category = ({ category, categories }) => {
-  const seo = {
-    metaTitle: category.name,
-    metaDescription: `All ${category.name} articles`,
-  }
+// const Category = ({ category, categories }) => {
+const Category = () => {
+  // const seo = {
+  //   metaTitle: category.name,
+  //   metaDescription: `All ${category.name} articles`,
+  // }
 
   return (
-    <Layout categories={categories}>
-      <Seo seo={seo} />
+    // <Layout categories={categories}>
+    <Layout>
+      {/* <Seo seo={seo} /> */}
       <div className="uk-section">
         <div className="uk-container uk-container-large">
           {/* <Box mb="30px"> */}
-            <h1>{category.name}</h1>
+            {/* <h1>{category.name}</h1> */}
+            <h1>Slug Page</h1>
           {/* </Box> */}
-          <Articles articles={category.articles} />
+          {/* <Articles articles={category.articles} /> */}
+          <Articles />
         </div>
       </div>
     </Layout>
   )
 }
 
-export async function getStaticPaths() {
-  const categories = await fetchAPI("/categories")
+// export async function getStaticPaths() {
+//   const categories = await fetchAPI("/categories")
 
-  return {
-    paths: categories.map((category) => ({
-      params: {
-        slug: category.slug,
-      },
-    })),
-    fallback: false,
-  }
-}
+//   return {
+//     paths: categories.map((category) => ({
+//       params: {
+//         slug: category.slug,
+//       },
+//     })),
+//     fallback: false,
+//   }
+// }
 
-export async function getStaticProps({ params }) {
-  const category = (await fetchAPI(`/categories?slug=${params.slug}`))[0]
-  const categories = await fetchAPI("/categories")
+// export async function getStaticProps({ params }) {
+//   const category = (await fetchAPI(`/categories?slug=${params.slug}`))[0]
+//   const categories = await fetchAPI("/categories")
 
-  return {
-    props: { category, categories },
-    revalidate: 1,
-  }
-}
+//   return {
+//     props: { category, categories },
+//     revalidate: 1,
+//   }
+// }
 
 export default Category
